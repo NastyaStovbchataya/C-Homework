b09814771bbf180df1b644b13a83f81f81d0c5a1
NastyaStovbchataya <90332773+NastyaStovbchataya@users.noreply.github.com>
2021-09-09 00:33:30 +0300

    Update ex3.cpp

--- ex3.cpp
+++ ex3.cpp
@@ -14,23 +14,17 @@ int main() {
 	std::cin >> c;
 	const double epsilon = std::numeric_limits <double> ::epsilon();
 	d = pow(b, 2) - 4 * a * c;
-	if ((abs(d) <= epsilon) and (abs(a) > epsilon)) {
-		x1 = -b / (2 * a);
-		if (abs(x1) <= epsilon) {
-			x1 = 0.0;
-			std::cout << "x = " << x1;
+
+	if (abs(a) <= epsilon) {
+		if (abs(b) <= epsilon) {
+			if (abs(c) <= epsilon) {
+				std::cout << "All real numbers";
+			}
+			else {
+				std::cout << "No solution";
+			}
 		}
 		else {
-			std::cout << "x = " << x1;
-		}
-	}
-	else if ((d > 0) and (abs(a) > epsilon)) {
-		x1 = (-b + sqrt(d)) / (2 * a);
-		x2 = (-b - sqrt(d)) / (2 * a);
-		std::cout << "x1 = " << x1 << "\n" << "x2 = " << x2;
-	}
-	else if (abs(a) <= epsilon) {
-		if (abs(b) >= epsilon) {
 			x1 = -c / b;
 			if (abs(x1) <= epsilon) {
 				x1 = 0.0;
@@ -40,15 +34,26 @@ int main() {
 				std::cout << "x = " << x1;
 			}
 		}
-		else if (abs(c) <= epsilon) {
-			std::cout << "All real numbers";
+	}
+	else if (abs(d) <= epsilon) {
+		x1 = -b / (2 * a);
+		if (abs(x1) <= epsilon) {
+			x1 = 0.0;
+			std::cout << "x = " << x1;
 		}
 		else {
-			std::cout << "No solution";
+			std::cout << "x = " << x1;
 		}
+	}
+	else if (d > 0) {
+		x1 = (-b + sqrt(d)) / (2 * a);
+		x2 = (-b - sqrt(d)) / (2 * a);
+		std::cout << "x1 = " << x1 << "\n" << "x2 = " << x2;
 	}
 	else {
 		std::cout << "No solution";
 	}
+
 	return 0;
+
 }
